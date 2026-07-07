43e9ef4f4f7e3edb5898b39c0187f6bb5494379a
Andrea Lops <h2o.al.97@gmail.com>
2026-07-07 09:31:16 +0200

    feat(ios-legacy): scaffold OpenSidecariOSLegacy target (iOS 12)

---
+++ iOS-Legacy/RootViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+final class RootViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
